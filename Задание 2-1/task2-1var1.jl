0110b496cb687d0707feb5764342e7cb261ca837
katia948 <80915237+katia948@users.noreply.github.com>
2021-06-05 15:04:40 +0300

    Add task 2-1

---
+++ Задание 2-1/task2-1var1.jl
@@ -0,0 +1,22 @@
+function getUserInput(msg="")
+    print("$msg ")
+      try
+        return parse(Float64,readline())
+      catch
+       println("-- Ошибка! Пожалуйста, введите число \n")
+       getUserInput(msg)
+      end
+    end
+
+# Получаем данные от пользователя
+r = getUserInput("Введите радиус шара \n\n");
+
+# Вычисляем объем шара
+V = 4/3 * pi * (r^3);
+
+# Вычисляем площадь шара
+S = 4 * pi * (r^2);
+
+# Выводим результаты вычислений на экран
+println("Площадь поверхности шара равна ", S);
+println("Объем шара равен ", V);
